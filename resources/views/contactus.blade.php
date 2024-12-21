117e88fdfce7088dacf5fae9f768de92b4292b93
jmlcoding <stepscoding196@gmail.com>
2024-12-21 16:49:10 +0200

    updated 10

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    @vite('resources/css/app.css')
     <title>Contact Us</title>
 </head>
 <body>
