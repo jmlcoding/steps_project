7b87d6aa021e7aba699e484024cab8dae0c6bab2
jmlcoding <stepscoding196@gmail.com>
2024-12-21 14:19:37 +0200

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Home</title>
 </head>
 <body>
-    <h1>Home</h1>
+    <h1 style="color: brown">Laravel</h1>
     <h2><a href="{{route('contact')}}">contactus</a></h2>
 </body>
 </html>
